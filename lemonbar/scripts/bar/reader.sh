6be259696620e009578a82243bde6b78f23fb120
Frank Plowman <fplowman1@gmail.com>
2019-10-23 16:25:35 +0100

    tidy up lemonbar script

--- lemonbar/scripts/bar/reader.sh
+++ lemonbar/scripts/bar/reader.sh
@@ -1,22 +1,5 @@
 #!/bin/env bash
 
-widgetdir="$HOME/scripts/bar"
-PANEL_FIFO="/tmp/lemon_fifo"
-
-[ -e "$PANEL_FIFO" ] && rm "$PANEL_FIFO"
-mkfifo "$PANEL_FIFO"
-
-$widgetdir/arch.sh > "$PANEL_FIFO" &
-$widgetdir/bspwm.sh > "$PANEL_FIFO" &
-$widgetdir/clock.sh > "$PANEL_FIFO" &
-$widgetdir/date.sh > "$PANEL_FIFO" &
-$widgetdir/power.sh > "$PANEL_FIFO" &
-$widgetdir/spotify.sh > "$PANEL_FIFO" &
-$widgetdir/volume_alsa.sh > "$PANEL_FIFO" &
-#$widgetdir/volume_pulse.sh > "$PANEL_FIFO" &
-#$widgetdir/network.sh > "$PANEL_FIFO" &
-#$widgetdir/battery.sh > "$PANEL_FIFO" &
-
 while read -r line; do
     case $line in
         A*)
@@ -52,4 +35,4 @@ while read -r line; do
     esac
     echo "%{l}${arch}${bspwm}%{c}${spotify}%{r}${volume_alsa} ${date} ${clock} ${power}"
 
-done < <(cat $PANEL_FIFO)
+done
