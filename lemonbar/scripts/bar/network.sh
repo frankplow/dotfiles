0d15d24fce05593ebd055574a639fca59480ebf0
Frank Plowman <fplowman1@gmail.com>
2019-10-24 18:16:55 +0100

    fix battery & network widgets

--- lemonbar/scripts/bar/network.sh
+++ lemonbar/scripts/bar/network.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-INFO=$(nmcli -t -f active,ssid,signal dev wifi | grep yes)
-SSID=$(echo $INFO | cut -d: -f2)
-CONN=$(echo $INFO | cut -d: -f3)
+while true
+do
+    INFO=$(nmcli -t -f active,ssid,signal dev wifi | grep yes)
+    SSID=$(echo $INFO | cut -d: -f2)
+    CONN=$(echo $INFO | cut -d: -f3)
 
-echo -n "I"
-if [ "$CONN" -lt 33 ]; then
-    echo -ne "\ue0ee "
-elif [ "$CONN" -lt 66 ]; then
-    echo -ne "\ue0ef "
-else
-    echo -ne "\ue0f0 "
-fi
-echo "$SSID "
+    echo -n "I"
+    if [ "$CONN" -lt 33 ]; then
+        echo -ne "\ue0ee "
+    elif [ "$CONN" -lt 66 ]; then
+        echo -ne "\ue0ef "
+    else
+        echo -ne "\ue0f0 "
+    fi
+    echo "$SSID "
+
+    sleep 10
+done
