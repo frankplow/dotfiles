7038cb62927e168eab0aa94173dff344782fb1cd
Frank Plowman <fplowman1@gmail.com>
2020-08-31 14:07:37 +0100

    switch to rofi power menu

--- lemonbar/scripts/bar/power.sh
+++ lemonbar/scripts/bar/power.sh
@@ -2,4 +2,4 @@
 
 . /home/frank/scripts/bar/colours.sh
 
-echo -e "E%{F$POWER}%{R}%{A:/home/frank/scripts/powermenu.sh:} \ue10c %{A}%{R}%{F-}";
+echo -e "E%{F$POWER}%{R}%{A:rofi -show power:} \ue10c %{A}%{R}%{F-}";
---
+++ rofi/.config/rofi/power.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+declare -A options
+options["poweroff"]="systemctl poweroff"
+options["reboot"]="grub-reboot 0 && systemctl reboot"
+options["reboot (windows)"]="grub-reboot 2 && systemctl reboot"
+
+for option in "${!options[@]}"; do
+    echo $option
+done
+
+for option in "${!options[@]}"; do
+    if [[ $1 == $option ]]; then
+        eval ${options[$option]}
+    fi
+done
