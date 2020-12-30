0840abb7afb94306f0cde6d88b68fc5a397bfcc9
Frank Plowman <fplowman1@gmail.com>
2020-12-30 16:40:27 +0000

    add VPN indicator

--- lemonbar/scripts/bar/network.sh
+++ lemonbar/scripts/bar/network.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 while true
 do
     INFO=$(nmcli -t -f active,ssid,signal dev wifi | grep yes)
+    VPN=$(nmcli -t -f state,name conn | grep activated | grep wg0 | wc -l)
     SSID=$(echo $INFO | cut -d: -f2)
     CONN=$(echo $INFO | cut -d: -f3)
-    RET="I"
 
+    RET="I"
     if [ "$CONN" -lt 33 ]; then
         RET="$RET\ue0ee "
     elif [ "$CONN" -lt 66 ]; then
@@ -14,8 +15,15 @@ do
     else
         RET="$RET\ue0f0 "
     fi
-    RET="$RET$SSID "
-    echo -e $RET
 
+    RET="$RET$SSID"
+
+    if [ "$VPN" ]; then
+       RET="$RET\u00b0"
+    else
+        RET="$RET "
+    fi
+
+    echo -e $RET
     sleep 10
 done
