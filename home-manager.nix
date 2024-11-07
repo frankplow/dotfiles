d2264b232d8d0764ee39eb47d79c186054e31970
Frank Plowman <frankplow@xiaomi.com>
2024-11-07 18:59:19 +0000

    nix: Add more applications

--- home-manager.nix
+++ home-manager.nix
@@ -3,8 +3,15 @@
 {
   home.packages = with pkgs; [
     alacritty
+    bat
+    pkgs-unstable.fzf
+    htop
+    ibm-plex
+    obsidian
+    pkgs-unstable.neovim
     net-news-wire
     raycast
+    rectangle
     ripgrep
     libsForQt5.yuview
     pkgs-unstable.zotero
