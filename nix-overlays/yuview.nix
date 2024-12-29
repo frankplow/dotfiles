9f30c0dee2c428dfadc6e67670e7bbd9dc97436f
Frank Plowman <post@frankplowman.com>
2024-12-29 11:16:29 +0000

    nix: Use YUView develop branch with some bugfixes

--- nix-overlays/yuview.nix
+++ nix-overlays/yuview.nix
@@ -1,13 +1,13 @@
 final: prev: {
   libsForQt5 = prev.libsForQt5.overrideScope (qt5final: qt5prev: {
     yuview = qt5prev.yuview.overrideAttrs (oldAttrs: rec {
-      version = "2.14";
+      version = "12a61c12401d991feab348c7c520233d604fd32d";
       patches = (oldAttrs.patches or []) ++ [ ./darwin-install-target.patch ];
       src = prev.fetchFromGitHub {
         owner = "IENT";
         repo = "YUView";
-        rev = "v${version}";
-        sha256 = "sha256-YuKPRYBr1CKrwickk1T89ZCYFt99jP86tdanp+JZMO4=";
+        rev = "${version}";
+        sha256 = "sha256-T2xLf1PA66xLLjZbHTnoJhZxBaPcEIOLQMB+5wgNgac=";
       };
     });
   });
